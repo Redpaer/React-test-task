c69fdf6a50f233751e124427c6b5f5e8edec6c91
Artem <52608393+Redpaer@users.noreply.github.com>
2021-04-22 05:16:27 +0300

    Update user.route.js

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,8 +3,24 @@ const User = require('../models/User');
 const router = Router();
 const {check, validationResult} = require('express-validator');
 
-router.post('/add', async (req,res) =>{
+router.post('/add',
+    [
+        check('name', 'Некорректное имя').isLength({min: 2}),
+        check('age', 'Некорректное имя').isLength({min: 1}),
+        check('city', 'Некорректное имя').isLength({min: 2}),
+        check('phone', 'Некорректное имя').isLength({min: 8})
+    ]
+    ,
+    async (req,res) =>{
         try{    
+            const errors = validationResult(req)
+            if(!errors.isEmpty()){
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'Некорректные данные'
+                })
+            }
+
             const {name, age, city, phone} = req.body
 
             const isUsed = await User.findOne({ phone })
